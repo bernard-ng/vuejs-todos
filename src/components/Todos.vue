0557e9c9037f97b3982c33590ad564039362849d
bernard-ng <ngandubernard@gmail.com>
2018-11-12 00:26:53 +0200

    [FEAT] init todo filters feature

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -16,6 +16,11 @@
         </div>
         <footer class="footer">
             <span class="todo-count"><strong>{{ remaing }}</strong> items left</span>
+            <ul class="filters">
+                <li><a href="#">All</a></li>
+                <li><a href="#">Active</a></li>
+                <li><a href="#">Completed</a></li>
+            </ul>
         </footer>
     </section>
 </template>
